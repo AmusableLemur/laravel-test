732b8ae2dc16114eddfede2a9ebd833e175cbdc9
Rasmus Larsson <rasmus@rasmuslarsson.se>
2014-02-24 10:36:53 +0100

    Updated profile page

--- app/views/profile.blade.php
+++ app/views/profile.blade.php
@@ -1,5 +1,11 @@
 @extends("layout")
 
 @section("content")
-	Hello, {{ $profile->name }}!
+	<legend>
+		{{ $profile->name }}
+	</legend>
+	@if ($profile->id == Auth::user()->id)
+		<a href="#">Redigera</a>
+	@endif
+	<h6>Medlem sedan {{ $profile->created_at }}</h6>
 @stop
